03843f85b8fc030fb62992e986c5159ff3f03525
YAS-CODE <12095418+YAS-CODE@users.noreply.github.com>
2019-11-24 12:19:13 +0500

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using WebrunsDemo.Servics;
+
+namespace WebrunsDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public readonly IMqttService _mqttService;
+        
+        public class UserRequst
+        {
+            public string dev { get; set; }
+            public string opr { get; set; }
+        }
+
+        public IndexModel(ILogger<IndexModel> logger, IMqttService mqttService)
+        {
+            _logger = logger;
+            _mqttService = mqttService;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<ActionResult> OnPostPostOperationAsync([FromBody] UserRequst operation)
+        {
+            if(operation.dev == "Mote")
+                await _mqttService.PublishAsync("/iot/zolertia/cmd", operation.opr);
+            else
+            {
+                if(operation.opr == "on")
+                    await _mqttService.PublishAsync("/iot/sensor/cmd", "ACTIVATETEMPSEN");
+                else
+                    await _mqttService.PublishAsync("/iot/sensor/cmd", "DEACTIVATETEMPSEN");
+            }
+                
+            return new JsonResult("Ok");
+        }
+    }
+}
